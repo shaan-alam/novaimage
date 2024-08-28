07dff34bbd573c567ae3891752b2bd3b3acdeac7
shaan-m0 <shaan@m0.ventures>
2024-08-29 02:42:19 +0530

    added tailwind temporary fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+// @ts-ignore
 import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 function addVariablesForColors({ addBase, theme }: any) {

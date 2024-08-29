c97001b31677ca2ce60d5b2e517c9b5d1217da34
shaan-m0 <shaan@m0.ventures>
2024-08-29 19:37:34 +0530

    basic dashboard dummy ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,20 +2,20 @@ import Sidebar from "@/components/shared/Sidebar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { getUser } from "../actions/user.actions";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
-  console.log(userId)
 
   const user = await getUser(userId);
 
   return (
-    <div className="bg-neutral-900 flex">
+    <main className="bg-neutral-900 flex h-screen">
       <Sidebar user={JSON.parse(JSON.stringify(user))} />
-      {children}
-    </div>
+      <ScrollArea className="h-full p-24 pb-8 pr-0 w-full">{children}</ScrollArea>
+    </main>
   );
 };
 

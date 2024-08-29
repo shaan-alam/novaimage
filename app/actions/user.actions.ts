b048ad67e0fc32b8ff78ea8afe64703a8ee56e5b
shaan-m0 <shaan@m0.ventures>
2024-08-29 15:42:59 +0530

    implemented prisma and clerk webhooks

---
+++ app/actions/user.actions.ts
@@ -0,0 +1,34 @@
+"use server";
+import { db } from "@/db";
+import { User } from "@prisma/client";
+
+export type CreateUserParams = Omit<User, "id">;
+export type UpdateUserParams = {
+  id: string;
+  user: Omit<User, "id" | "email" | "clerkId">;
+};
+
+export const createUser = async (user: CreateUserParams) => {
+  return await db.user.create({
+    data: { ...user },
+  });
+};
+
+export const updateUser = async ({ id, user }: UpdateUserParams) => {
+  return await db.user.update({
+    where: {
+      id,
+    },
+    data: {
+      ...user,
+    },
+  });
+};
+
+export const deleteUser = async (id: string) => {
+  return await db.user.delete({
+    where: {
+      id
+    }
+  })
+}

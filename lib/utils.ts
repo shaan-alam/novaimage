f6546102a8ba3b63d75cab0b608019f944a1527d
shaan-m0 <shaan@m0.ventures>
2024-09-04 16:18:03 +0530

    Added a skeleton to transformed image

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,12 +28,12 @@ const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#7986AC" offset="20%" />
-      <stop stop-color="#68769e" offset="50%" />
-      <stop stop-color="#7986AC" offset="70%" />
+      <stop stop-color="#182027" offset="20%" />
+      <stop stop-color="#26333D" offset="50%" />
+      <stop stop-color="#182027" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect width="${w}" height="${h}" fill="#182027" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;

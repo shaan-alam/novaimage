6555a172f43b98b0ebfdd2b020c02c540cad5b0b
shaan-m0 <shaan@m0.ventures>
2024-10-02 15:34:15 +0530

    added animation to transformations card

--- components/Transformation/TransformationCard.tsx
+++ components/Transformation/TransformationCard.tsx
@@ -1,13 +1,25 @@
+import { useEffect, useRef, useState } from "react";
 import { Card } from "@/components/ui/card";
 import { Recolor, Transformation } from "@prisma/client";
 import { CldImage } from "next-cloudinary";
 import Link from "next/link";
+import { motion, useInView } from "framer-motion";
 
 type TransformationCardProps = {
   transformation: Transformation & { recolor: Recolor | null };
 };
 
 const TransformationCard = ({ transformation }: TransformationCardProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  useEffect(() => {
+    if (isInView) {
+      setHasAnimated(true);
+    }
+  }, [isInView]);
+
   const config = {
     recolor: {
       to: transformation?.recolor?.to || "",
@@ -26,30 +38,52 @@ const TransformationCard = ({ transformation }: TransformationCardProps) => {
   };
 
   return (
-    <Link
-      href={`/${transformationLinks[transformation?.transformationType as keyof typeof transformationLinks]}/update/${transformation.id}`}
+    <motion.div
+      ref={ref}
+      initial={{
+        scale: 0.85,
+        opacity: 0,
+        filter: "blur(10px)",
+      }}
+      animate={
+        hasAnimated
+          ? {
+              scale: 1,
+              opacity: 1,
+              filter: "blur(0px)",
+            }
+          : {}
+      }
+      transition={{
+        duration: 0.5,
+        ease: "easeOut",
+      }}
     >
-      <Card className="relative h-[300px] w-[300px] group cursor-pointer hover:scale-105 transition-all">
-        <CldImage
-          src={transformation.publicId}
-          height={150}
-          width={150}
-          alt={transformation.title}
-          {...config}
-          className="rounded-tl-md rounded-tr-md absolute top-0 left-0 object-cover h-full w-full"
-        />
-        <div className="absolute h-full w-full bg-gradient-to-t from-black/90 to-transparent z-10 transition-all opacity-0 group-hover:opacity-100"></div>
-        <div>
-          <h1 className="text-lg text-white font-medium opacity-0 absolute bottom-4 left-4 z-20 transition-all group-hover:opacity-100">
-            {transformation.title.length != 0 ? (
-              transformation.title
-            ) : (
-              <span className="italic">&lt;No Title&gt;</span>
-            )}
-          </h1>
-        </div>
-      </Card>
-    </Link>
+      <Link
+        href={`/${transformationLinks[transformation?.transformationType as keyof typeof transformationLinks]}/update/${transformation.id}`}
+      >
+        <Card className="relative h-[300px] w-[300px] group cursor-pointer hover:scale-105 transition-all">
+          <CldImage
+            src={transformation.publicId}
+            height={150}
+            width={150}
+            alt={transformation.title}
+            {...config}
+            className="rounded-tl-md rounded-tr-md absolute top-0 left-0 object-cover h-full w-full"
+          />
+          <div className="absolute h-full w-full bg-gradient-to-t from-black/90 to-transparent z-10 transition-all opacity-0 group-hover:opacity-100"></div>
+          <div>
+            <h1 className="text-lg text-white font-medium opacity-0 absolute bottom-4 left-4 z-20 transition-all group-hover:opacity-100">
+              {transformation.title.length != 0 ? (
+                transformation.title
+              ) : (
+                <span className="italic">&lt;No Title&gt;</span>
+              )}
+            </h1>
+          </div>
+        </Card>
+      </Link>
+    </motion.div>
   );
 };
 
